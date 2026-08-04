5afee87371bbfca15150110a705aeb3fb6b9f025
beezee05 <111074638+beezee05@users.noreply.github.com>
2026-08-04 16:21:40 -0400

    Update brians_atmos_watchlist.py

--- brians_atmos_watchlist.py
+++ brians_atmos_watchlist.py
@@ -1,1 +1,7 @@
-
+# Brian's Atmos Watchlist
+# Filtered Dolby Atmos / remaster RSS feed
+#
+# The full feed generator will be added next.
+#
+# Source:
+# https://spatialaudiodb.com/rss/dolby-atmos.xml
